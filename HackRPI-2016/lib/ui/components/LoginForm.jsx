a81c4664e641477ea8291d0632bb67d8d7f08051
Philip Bedward Jr <philipbed813@gmail.com>
2016-11-13 05:43:24 -0500

    added styles

--- HackRPI-2016/lib/ui/components/LoginForm.jsx
+++ HackRPI-2016/lib/ui/components/LoginForm.jsx
@@ -1,6 +1,7 @@
 import React ,{ Component } from 'react';
 import ReactDOM from 'react-dom';
 
+
 export default class Home extends Component{
 
     handleSubmit(e){
@@ -18,12 +19,11 @@ export default class Home extends Component{
 
     render(){
         return(
-            <div>
+            <div className="col-sm-6 col-md-offset-3 mainDiv">
                 <form onSubmit={this.handleSubmit.bind(this)}>
                     <div className="form-group">
                         <div className="row">
-                            <label className="col-sm-2 control-label col-sm-offset-4">Username or Email:</label>
-                            <div className="col-sm-2">
+                            <div className="col-sm-5 col-sm-offset-4">
                                 <input
                                     className="form-control"
                                     placeholder="Enter Username or Email"
@@ -34,8 +34,8 @@ export default class Home extends Component{
                         </div>
 
                         <div className="row">
-                            <label className="col-sm-2 control-label col-sm-offset-4">Password:</label>
-                            <div className="col-sm-2">
+
+                            <div className="col-sm-5 col-sm-offset-4">
                                 <input
                                     className="form-control"
                                     placeholder="Enter Password"
@@ -45,7 +45,7 @@ export default class Home extends Component{
                             </div>
                         </div>
                         <div className="row text-center">
-                            <button type="submit" className="btn btn-primary">Login</button>
+                            <button type="submit" className="btn btn-primary col-sm-offset-1">Login</button>
                             <a onClick={this.cancel.bind(this)} className="btn btn-danger col-sm-offset-1">Cancel</a>
                         </div>
                     </div>
--- HackRPI-2016/server/main.js
+++ HackRPI-2016/server/main.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import '../lib/api/auth.js';
-import '../lib/api/importData.js';
+import '../lib/api/dataService.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
