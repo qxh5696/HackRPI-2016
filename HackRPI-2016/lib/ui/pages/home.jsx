2eb84ad827e4a097c89d00bf91f1ee3a8bae738f
rachaelthormann <rachaelthormann@gmail.com>
2016-11-13 05:12:40 -0500

    Navbar links should work.

--- HackRPI-2016/lib/ui/pages/home.jsx
+++ HackRPI-2016/lib/ui/pages/home.jsx
@@ -1,14 +1,24 @@
 import React, {Component} from 'react';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import './home.css';
 
 export default class HomePage extends Component{
+
+    loginHandleClick(event){
+        event.preventDefault();
+        FlowRouter.go('/login');
+    }
+
+    registerHandleClick(event){
+        event.preventDefault();
+        FlowRouter.go('/register');
+    }
 
     navbar(){
         var navBarInstance = (
             <ul>
-                <li><a href="default.asp">Home</a></li>
-                <li><a href="news.asp">News</a></li>
-                <li><a href="contact.asp">Contact</a></li>
-                <li><a href="about.asp">About</a></li>
+                <li><a onClick={this.loginHandleClick.bind(this)}>Login</a></li>
+                <li ><a onClick={this.registerHandleClick.bind(this)}>Register</a></li>
             </ul>
         );
 
